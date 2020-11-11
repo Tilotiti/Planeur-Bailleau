2bb63008281392c6c98c984e572dd9c90d9e2bfd
Thibault HENRY <thibault@fidcar.com>
2020-11-11 10:17:48 +0100

    upgrade

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class CreateAdminCommand extends Command
+{
+    protected static $defaultName = 'app:create-admin';
+
+    /**
+     * @var UserRepository
+     */
+    private UserRepository $userRepository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private UserPasswordEncoderInterface $userPasswordEncoder;
+
+    public function __construct(
+        UserRepository $userRepository,
+        EntityManagerInterface $entityManager,
+        UserPasswordEncoderInterface $userPasswordEncoder
+    )
+    {
+        parent::__construct();
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $io->writeln('Vous allez pouvoir créer un administrateur pour vous connecter au site local :');
+
+        $firstname = $io->ask('Prénom ?', 'Dominique');
+
+        $lastname = $io->ask('Nom ?', 'Strauss-Kahn');
+
+        $email = $io->ask('E-mail de connexion', 'dsk@pornhub.com', function(string $email) {
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                throw new \RuntimeException("Un E-mail ... C'est le truc avec le a bizarre ...
+                ");
+            }
+
+            $exists = $this->userRepository->findOneByEmail($email);
+
+            return $email;
+        });
+
+        $password = $io->askHidden('Mot de passe ?', function($password) {
+            if(!preg_match(User::PASSWORD_REGEX, $password)) {
+                throw new \RuntimeException('Franchement ... au moins 8 caractères, un chiffre et une lettre ... Fais un effort ...');
+            }
+
+            return $password;
+        });
+
+        $user = new User();
+        $user->setFirstname($firstname);
+        $user->setLastname($lastname);
+        $user->setEmail($email);
+
+        $user->setPassword(
+            $this->userPasswordEncoder->encodePassword($user, $password)
+        );
+
+        $io->success("C'est tout bon ! Tu peux maintenant te connecter depuis l'adresse suivante :");
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ */
+class User implements UserInterface
+{
+    /**
+     * @var integer
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private ?string $email = null;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private array $roles = [self::ROLE_USER];
+
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+    const ROLE_USER = 'ROLE_USER';
+
+    /**
+     * @var ?string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private ?string $password;
+
+    const PASSWORD_REGEX = '#^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$#i';
+
+    /**
+     * @var string|null
+     */
+    private ?string $plainPassword = null;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $firstname = '';
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $lastname = '';
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetimetz")
+     */
+    private \DateTime $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetimetz")
+     */
+    private \DateTime $updatedAt;
+
+    /**
+     * @var \DateTime|null
+     * @ORM\Column(type="datetimetz")
+     */
+    private ?\DateTime $connectedAt = null;
+
+    public function __construct() {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string|null $email
+     */
+    public function setEmail(?string $email): void
+    {
+        $this->email = trim(mb_strtolower($email));
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = self::ROLE_USER;
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    /**
+     * @param string|null $plainPassword
+     */
+    public function setPlainPassword(?string $plainPassword): void
+    {
+        $this->plainPassword = $plainPassword;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        $this->plainPassword = null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = ucfirst(mb_strtolower($firstname));
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = mb_strtoupper($lastname);
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt(\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt(): \DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt(\DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getConnectedAt(): ?\DateTime
+    {
+        return $this->connectedAt;
+    }
+
+    /**
+     * @param \DateTime|null $connectedAt
+     */
+    public function setConnectedAt(?\DateTime $connectedAt): void
+    {
+        $this->connectedAt = $connectedAt;
+    }
+}
