810c3cbb429ced5f343cec5ab149a33dbba96246
Thibault HENRY <thibault@fidcar.com>
2021-05-10 12:31:02 +0200

    page documents

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Aircraft;
 use App\Entity\Page;
 use App\Repository\AircraftRepository;
+use App\Repository\DocumentCategoryRepository;
+use App\Repository\DocumentRepository;
 use App\Repository\PageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +15,16 @@ class PageController extends AbstractController
 {
     private AircraftRepository $aircraftRepository;
     private PageRepository $pageRepository;
+    private DocumentCategoryRepository $documentCategoryRepository;
 
     public function __construct(
         AircraftRepository $aircraftRepository,
-        PageRepository $pageRepository
+        PageRepository $pageRepository,
+        DocumentCategoryRepository $documentCategoryRepository
     ) {
         $this->aircraftRepository = $aircraftRepository;
         $this->pageRepository = $pageRepository;
+        $this->documentCategoryRepository = $documentCategoryRepository;
     }
 
     /**
@@ -40,6 +45,24 @@ class PageController extends AbstractController
         return $this->render('default/aircrafts.html.twig', [
             'page' => $page,
             'aircrafts' => $aircrafts
+        ]);
+    }
+
+    /**
+     * @Route("/documents" , name="documents")
+     * @param Page $page
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function documents() {
+        $page = $this->pageRepository->findOneByCode('documents');
+
+        $categories = $this->documentCategoryRepository->findBy([], [
+            'title' => 'ASC'
+        ]);
+
+        return $this->render('default/documents.html.twig', [
+            'page' => $page,
+            'categories' => $categories
         ]);
     }
 }
